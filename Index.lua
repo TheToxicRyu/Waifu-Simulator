d07cf4556a2d6073c3b6a19e540875b76f9412e1
TheToxicRyu <toxicmario6421@gmail.com>
2015-12-31 22:35:43 +1000

    Lua Script Modified Slightly

--- Index.lua
+++ Index.lua
@@ -1,10 +1,12 @@
 background = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_TopScreen.bmp") 
 defaultfont = Font.load(System.currentDirectory().."/GameData/Font/Default.ttf")
 
+
 while true do 
-    Screen.waitVblankStart()
+    pad = Controls.read()
 	Screen.refresh()
 	Screen.drawImage(0,0,background,TOP_SCREEN)
 	Screen.flip()
+	Screen.waitVblankStart()
 	Font.print(defaultfont, 5, 5, "Wafui Simulator Debug Screen!", Color.new(255,255,255), BOTTOM_SCREEN)
 end
