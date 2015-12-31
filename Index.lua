51c00b617035e8ed0c07155a24ad988a2256d734
TheToxicRyu <toxicmario6421@gmail.com>
2015-12-31 22:47:28 +1000

    Added BottomScreen Background Debug Text not visable

--- Index.lua
+++ Index.lua
@@ -1,4 +1,5 @@
-background = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_TopScreen.bmp") 
+background = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_TopScreen.bmp")
+backgroundb = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_BottomScreen.bmp") 
 defaultfont = Font.load(System.currentDirectory().."/GameData/Font/Default.ttf")
 
 
@@ -6,6 +7,7 @@ while true do
     pad = Controls.read()
 	Screen.refresh()
 	Screen.drawImage(0,0,background,TOP_SCREEN)
+	Screen.drawImage(0,0,backgroundb,BOTTOM_SCREEN)
 	Screen.flip()
 	Screen.waitVblankStart()
 	Font.print(defaultfont, 5, 5, "Wafui Simulator Debug Screen!", Color.new(255,255,255), BOTTOM_SCREEN)
