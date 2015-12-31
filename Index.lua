15771763ba29ce937f214480d55645f1a3e3c232
TheToxicRyu <toxicmario6421@gmail.com>
2015-12-31 19:52:35 +1000

    Added some scripts to hopefully load the title screen background

--- Index.lua
+++ Index.lua
@@ -1,5 +1,10 @@
 background = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_ScreenTop1.bmp") 
+Screen.flipImage(bitmap2,bitmap)
+Screen.fillRect(50,150,1,15,white,bitmap2)
 
 while true do 
+    Screen.waitVblankStart()
+	Screen.refresh()
 	screen.drawImage(10,20,background,TOP_SCREEN) 
+	Screen.flip()
 end
