c351ca35550233c625e7250ba13fccb2082da43a
TheToxicRyu <toxicmario6421@gmail.com>
2015-12-31 23:09:44 +1000

    Added string to show start button, However the texture for the button doesn't show propely.

--- Index.lua
+++ Index.lua
@@ -1,5 +1,6 @@
 background = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_TopScreen.bmp")
 backgroundb = Screen.loadImage(System.currentDirectory().."/GameData/Backgrounds/Title_BottomScreen.bmp") 
+startbutton = Screen.loadImage(System.currentDirectory().."/GameData/Sprites/Title_StartPlaceholder.bmp") 
 defaultfont = Font.load(System.currentDirectory().."/GameData/Font/Default.ttf")
 
 
@@ -8,6 +9,7 @@ while true do
 	Screen.refresh()
 	Screen.drawImage(0,0,background,TOP_SCREEN)
 	Screen.drawImage(0,0,backgroundb,BOTTOM_SCREEN)
+	Screen.drawImage(0,0,startbutton,BOTTOM_SCREEN)
 	Screen.flip()
 	Screen.waitVblankStart()
 	Font.print(defaultfont, 5, 5, "Wafui Simulator Debug Screen!", Color.new(255,255,255), BOTTOM_SCREEN)
